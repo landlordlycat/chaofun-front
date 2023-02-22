61caf44382584c89f6ebb6be5aec13624d4527b7
cijianzy <cijianzy@gmail.com>
2023-02-22 19:32:10 +0800

    fix: 组件化

--- src/views/scratch/HomePage.vue
+++ src/views/scratch/HomePage.vue
@@ -20,49 +20,10 @@
       <div v-if="showHotTags" style="display: flex; flex-wrap: wrap; border: 1px solid gray ">
         <div v-for="(item,index) in tags" @click="goTag(item)" style="display: block; padding-right: 10px; color: blue; font-size: 20px;">{{item}}</div>
       </div>
-      <el-radio-group v-model="sort" style="margin-bottom: 20px;margin-top: 20px;" @change="changeSort">
-        <el-radio-button label="hot">最热</el-radio-button>
-        <el-radio-button label="new">最新</el-radio-button>
-        <el-radio-button label="mine">我创建的</el-radio-button>
-      </el-radio-group>
-
-      <el-pagination
-          background
-          layout="prev, pager, next"
-          style="padding-bottom: 20px"
-          :current-page.sync="current"
-          :page-size="50"
-          @current-change="handleCurrentChange"
-          :total="total">
-      </el-pagination>
-
-
-
-      <div style="display: flex; padding-bottom: 8px" v-for="(item, index) in list" @click="gotoGuess(item)" class="item">
-        <div  class="item-img">
-          <img class="cover" :src="imgOrigin + item.cover + '?x-oss-process=image/resize,h_300/format,jpeg/quality,q_75'" style="">
-          </img>
-        </div>
-
-        <div  class="item-contain">
-          <div style="font-size: 24px; font-weight: bold;">
-            {{item.name}}
-          </div>
-          <div>
-            {{item.desc}}
-          </div>
-        </div>
-      </div>
-
-      <el-pagination
-          background
-          layout="prev, pager, next"
-          style="padding-top: 20px"
-          :current-page.sync="current"
-          :page-size="50"
-          @current-change="handleCurrentChange"
-          :total="total">
-      </el-pagination>
+      <scratch-list
+          :sort="sort"
+          :current="current"
+      ></scratch-list>
     </section>
   </div>
 </template>
@@ -70,10 +31,11 @@
 <script>
 import * as api from '../../api/api'
 import page from "../permission/page";
+import ScratchList from "./scratch-list";
 
 export default {
   name: "HomePage",
-
+  components: {ScratchList},
   data() {
     return {
       totalTimes: null,
@@ -86,7 +48,7 @@ export default {
       list: [],
     }
   },
-  mounted() {
+  created() {
     if (this.$route.query.sort) {
       this.sort = this.$route.query.sort;
     }
@@ -97,19 +59,11 @@ export default {
       this.current = parseInt(this.$route.query.page);
     }
 
-    this.getList();
     this.listTag();
     this.getTotalGuessTimes();
   },
 
   methods: {
-    getList() {
-      api.getByPath('/api/v0/scratch/game/listV1', {order: this.sort, pageSize: 50, pageNum: this.current}).then(res=>{
-        this.list = res.data.games;
-        this.total = res.data.total;
-      })
-    },
-
     getTotalGuessTimes() {
       api.getByPath('/api/v0/scratch/game/getTotalStartTimes' ).then(res=>{
         this.totalTimes = res.data;
@@ -123,11 +77,6 @@ export default {
     goTag(item) {
       window.location.href = '/scratch/tag?tagName=' + item;
     },
-
-    gotoGuess(item) {
-      window.location.href = '/scratch/guess?id=' + item.id;
-    },
-
     toCreate() {
       this.doLoginStatus().then((res) => {
         if (res) {
@@ -135,23 +84,10 @@ export default {
         }
       });
     },
-
     random() {
       api.getByPath('/api/v0/scratch/game/random').then(res=>{
         window.location.href = '/scratch/guess?id=' + res.data;
       })
-    },
-
-    changeSort(tab, event) {
-      this.list = [];
-      this.current = 1;
-      this.$router.replace({query: {sort: this.sort, page: this.current  }})
-      this.getList();
-    },
-
-    handleCurrentChange(current) {
-      this.$router.replace({query: {sort: this.sort, page: this.current}})
-      this.getList()
     },
     gotoSearch() {
       window.location.href = '/scratch/search'
@@ -168,33 +104,15 @@ export default {
     max-width: 40%;
     margin: auto;
   }
-  .item {
-    .item-img {
-      flex-shrink: 0;
-      height: 100px;
-      width: 100px;
-    }
-    .item-contain {
-      padding-left: 8px;
-      flex-shrink: 1;
-    }
-    .cover {
-      width: 100%; height: 100%;
-      //background-color: grey;
-      object-fit: contain;
-    }
-  }
 
 }
 
 @media only screen and (max-width: 679px) {
   .container {
-
     .list_container {
       max-width: 90%;
       margin: auto;
     }
-
     .container {
     }
   }
---
+++ src/views/scratch/scratch-list.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <el-radio-group v-model="sort" style="margin-bottom: 20px;margin-top: 20px;" @change="changeSort">
+      <el-radio-button label="hot">最热</el-radio-button>
+      <el-radio-button label="new">最新</el-radio-button>
+      <el-radio-button label="mine">我创建的</el-radio-button>
+    </el-radio-group>
+
+    <el-pagination
+        background
+        layout="prev, pager, next"
+        style="padding-bottom: 20px"
+        :current-page.sync="current"
+        :page-size="50"
+        @current-change="handleCurrentChange"
+        :total="total">
+    </el-pagination>
+
+    <div style="display: flex; padding-bottom: 8px" v-for="(item, index) in list" @click="gotoGuess(item)" class="item">
+      <div  class="item-img">
+        <img class="cover" :src="imgOrigin + item.cover + '?x-oss-process=image/resize,h_300/format,jpeg/quality,q_75'" style="">
+        </img>
+      </div>
+
+      <div  class="item-contain">
+        <div style="font-size: 24px; font-weight: bold;">
+          {{item.name}}
+        </div>
+        <div>
+          {{item.desc}}
+        </div>
+      </div>
+    </div>
+
+    <el-pagination
+        background
+        layout="prev, pager, next"
+        style="padding-top: 20px"
+        :current-page.sync="current"
+        :page-size="50"
+        @current-change="handleCurrentChange"
+        :total="total">
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+import * as api from "../../api/api";
+
+export default {
+  name: "scratch-list",
+  data() {
+    return {
+      total: 10000,
+      list: [],
+      // sort: 'new',
+      // current: 1;
+    }
+  },
+  props: {
+    sort: String,
+    current: Number,
+    tag: String,
+    userId: Number,
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    getList() {
+      api.getByPath('/api/v0/scratch/game/listV1', {order: this.sort, pageSize: 50, pageNum: this.current, userId: this.userId, tag: this.tag}).then(res=>{
+        this.list = res.data.games;
+        this.total = res.data.total;
+      })
+    },
+    gotoGuess(item) {
+      window.location.href = '/scratch/guess?id=' + item.id;
+    },
+
+    changeSort(tab, event) {
+      this.list = [];
+      this.current = 1;
+      this.$router.replace({query: {sort: this.sort, page: this.current  }})
+      this.getList();
+    },
+
+    handleCurrentChange(current) {
+      this.$router.replace({query: {sort: this.sort, page: this.current}})
+      this.getList()
+    },
+    gotoSearch() {
+      window.location.href = '/scratch/search'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.item {
+  .item-img {
+    flex-shrink: 0;
+    height: 100px;
+    width: 100px;
+  }
+  .item-contain {
+    padding-left: 8px;
+    flex-shrink: 1;
+  }
+  .cover {
+    width: 100%; height: 100%;
+    //background-color: grey;
+    object-fit: contain;
+  }
+}
+
+</style>
