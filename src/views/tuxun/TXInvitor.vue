229c0de866796aa1c46fe492878bd6b8699d1b84
cijianzy <cijianzy@gmail.com>
2023-03-11 14:49:54 +0800

    fix: 增加 pano changed

--- src/views/tuxun/TXInvitor.vue
+++ src/views/tuxun/TXInvitor.vue
@@ -877,6 +877,12 @@ export default {
                         }
                     );
                     this.viewer.registerPanoProvider(this.getCustomPanorama)
+                    this.viewer.addListener("pano_changed", () => {
+                      console.log("pano_changed");
+                      if (this.viewer.getPano().length === 27) {
+                        this.getPanoInfo(this.viewer.getPano());
+                      }
+                    });
                     this.setGoogle(this.lastRound.panoId);
                   })
                 } else {
