a68e818651ac1e8c57a2c10b14feb429fb7a7776
cijianzy <cijianzy@gmail.com>
2023-03-12 19:23:22 +0800

    fix: 还是显示出来方便一些

--- src/views/scratch/GamePage.vue
+++ src/views/scratch/GamePage.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="back_home">
       <el-button v-if="history && history.length !== 1" @click="goBack" size="small" round>←返回</el-button>
-      <el-button v-else @click="goHome" size="small" round>←小测验首页</el-button>
+      <el-button @click="goHome" size="small" round>←小测验首页</el-button>
       <el-button @click="share" size="small"  round>分享</el-button>
       <el-button @click="random" size="small"  round>随机下一个</el-button>
       <el-button size="small"  v-if="guessInfo &&  this.$store.state.user && this.$store.state.user.userInfo  && this.$store.state.user.userInfo.userId === this.guessInfo.userId" @click="modify" round>修改</el-button>
