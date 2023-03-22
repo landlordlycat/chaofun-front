4f073fb1fc8001ae2e127f688ffe7ee0e3146532
cijianzy <cijianzy@gmail.com>
2023-03-22 17:25:43 +0800

    fix: 新人赛

--- src/views/tuxun/TXNewHome.vue
+++ src/views/tuxun/TXNewHome.vue
@@ -14,7 +14,7 @@
           <el-button  @click="toRank" >积分排行</el-button>
         </div>
       </div>
-      <div class="info" @click="toFirstTournament">   图寻新人赛（玩家第二届个人赛）圆满结束！</div>
+      <div class="info" @click="toFirstTournament"> 新人赛（玩家第二届个人赛）圆满结束！</div>
       <div class="second-info" @click="raiseVIP" v-if="$store.state.user.userInfo.userId !== 54242 && $store.state.user.islogin"> 会员费用下月即将停止优惠，将会调至「9.99/月」「69.99/年」欲购从速 </div>
       <div v-if="times" class="times">总轮次数：<span style="font-size: 18px">{{times}}</span></div>
       <section class="game_entrance">
